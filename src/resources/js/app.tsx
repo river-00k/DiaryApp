054a49097beb049f00d1defbe632125a5746b02a
Kohei <nisijinhokuryou@gmail.com>
2022-06-22 22:32:28 +0900

    app.tsx merged

--- src/resources/js/app.tsx
+++ src/resources/js/app.tsx
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { AuthProvider } from './auth/AuthContext';
-import ProtectedRoute from './auth/ProtectedRoute';
-import Example from './pages/Example';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import RegisterInputForm from './components/RegisterInputForm'
-import RegisterConfirmForm from './components/RegisterConfirmForm'
-import RegisterCompleteForm from './components/RegisterCompleteForm'
-
-const App = () => {
-
-    return(
-        <BrowserRouter>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element ={< LoginPage/>} />
-                    <Route path="/register" element ={< RegisterPage/>} >
-                        <Route path="input" element={<RegisterInputForm/>}/>
-                        <Route path="confirm" element={<RegisterConfirmForm/>}/>
-                        <Route path="complete" element={<RegisterCompleteForm/>}/>
-                    </Route>
-                    <Route path="/mypage" element={<ProtectedRoute/>}>
-                        <Route path="example" element ={< Example/>} />
-                    </Route>
-                </Routes>
-            </AuthProvider>
-        </BrowserRouter>
-    )
-
-}
-
-if (document.getElementById("app")){
-    ReactDOM.render(<App />, document.getElementById("app"))
-}
-=======
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes React and other helpers. It's a great starting point while
@@ -54,7 +15,7 @@ if (document.getElementById("app")){
  import React from 'react';
  import ReactDOM from 'react-dom';
  import { BrowserRouter, Route ,Routes} from "react-router-dom";
- import DiaryRoutes from "./DiaryRoutes";
+ import AppRoutes from "./AppRoutes";
  import './app.css'
  import Content from "./components/Content"
  import Example from "./pages/Example"
@@ -88,7 +49,7 @@ if (document.getElementById("app")){
               className="mb-8"
             />
             <Content>
-                <DiaryRoutes />
+                <AppRoutes />
             </Content>
         </Container>
       </Background>
@@ -100,4 +61,3 @@ if (document.getElementById("app")){
 
 ReactDOM.render(<App />, document.getElementById("app"));
 
->>>>>>> main
