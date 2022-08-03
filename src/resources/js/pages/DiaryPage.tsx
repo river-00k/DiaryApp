180aca63276ac0e2ce280475c9b7abf793515987
a0163072 <shotatsutom@gmail.com>
2022-08-03 21:58:00 +0900

    ハンバーガーメニュー実装

--- src/resources/js/pages/DiaryPage.tsx
+++ src/resources/js/pages/DiaryPage.tsx
@@ -6,9 +6,56 @@
  import GlobalStyle from '../styles/GlobalStyle'
  import Background from '../layout/Background'
 import { DiaryProvider } from '../contexts/DiaryContext';
+import { slide as Menu } from 'react-burger-menu'
 
 
  const DiaryPage = () => {
+
+    const styles = {
+        bmBurgerButton: {
+          position: 'fixed',
+          width: '36px',
+          height: '30px',
+          left: '36px',
+          top: '36px'
+        },
+        bmBurgerBars: {
+          background: '#373a47'
+        },
+        bmBurgerBarsHover: {
+          background: '#a90000'
+        },
+        bmCrossButton: {
+          height: '24px',
+          width: '24px'
+        },
+        bmCross: {
+          background: '#bdc3c7'
+        },
+        bmMenuWrap: {
+          position: 'fixed',
+          height: '100%'
+        },
+        bmMenu: {
+          background: '#373a47',
+          padding: '2.5em 1.5em 0',
+          fontSize: '2.0em',
+          fontWeight: 'bold'
+        },
+        bmMorphShape: {
+          fill: '#373a47'
+        },
+        bmItemList: {
+          color: '#b8b7ad',
+          padding: '0.8em'
+        },
+        bmItem: {
+          display: 'block'
+        },
+        bmOverlay: {
+          background: 'rgba(0, 0, 0, 0.3)'
+        }
+      }
 
     // const addProduct = useCallback(
     //     product => {
@@ -44,7 +91,11 @@ import { DiaryProvider } from '../contexts/DiaryContext';
     // );
 
     return (
-        <>
+        <>  <Menu styles={ styles } >
+            <a id="login" className="menu-item" href="/login">Logout</a>
+            <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+            </Menu>
             <GlobalStyle />
             {/* <Background> */}
                 <Container>
