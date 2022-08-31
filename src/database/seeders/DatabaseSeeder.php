064d893a7f165f453e8549da1ca1c7bf25fd77f7
Kohei <nisijinhokuryou@gmail.com>
2022-09-01 07:12:24 +0900

    Evaluation column connected to database

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(DiariesTableSeeder::class);
+        //$this->call(DiariesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 
     }
