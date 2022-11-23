f7c49e35de517256a8e86cac01eef468fe2e0fb3
Kohei <nisijinhokuryou@gmail.com>
2022-11-23 20:37:19 +0900

    implemented cron job

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\User;
+use App\Models\Diary;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,7 +18,34 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            //ゲストユーザーの一覧を取得する
+            $users = User::where('is_guest', 1)->get();
+
+            //ゲストユーザーアカウントとそれに紐づく日記を削除する
+            foreach ( $users as $user )
+            {
+                //ゲストユーザーを削除する
+                User::where('id', $user->id)->delete();
+
+                //日記を削除する
+                Diary::where('user_id', $user->id)->delete();
+            }
+            
+        });
     }
+
+    /**
+     * スケジュールされたイベントで使用するデフォルトタイムゾーンの取得
+     * 
+     * @return \DateTimeZone | String | null
+     */
+
+     protected function scheduleTimezone()
+     {
+         return 'Asia/Tokyo';
+     }
 
     /**
      * Register the commands for the application.
